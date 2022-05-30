5e2e2b7b39f1349b3e2b727ac5eafd19c79624fa
Bonjour-amanda <amandathecoders@gmail.com>
2022-05-30 10:38:43 +0700

    add mock and spy for function test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,13 @@ import App from './App';
 import { render, screen, userEvent } from './utils/test-utils';
 
 describe('Sample App vitest', () => {
-// describe - Used to group the test and used to describe what is currently being tested
+// describe -> Used to group the test and used to describe what is currently being tested
     
     it('the title is visible', () => {
-        // it or test - Individual test which is run by Vitest. It can either pass or fail
+        // it or test -> Individual test which is run by Vitest. It can either pass or fail
         render(<App />);
         expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument();
+        // expect -> is used to create assertions. In this context assertions are functions that can be called to assert a statement. 
     });
 
     it('should increment the count when icon (+) clicked', async() => {
---
+++ src/test/mockAndSpy.test.tsx
@@ -0,0 +1,36 @@
+import { describe, it, expect, vi } from "vitest";
+
+const player = {
+    name: 'Link',
+    level: 10,
+    posX: 0,
+    posY: 0,
+    move: function(x:number, y:number) {
+        this.posX = x;
+        this.posY = y;
+    }
+};
+
+//spy
+describe('Player Movement with spy', () => {
+    it('should move the player', () => {
+        const spy = vi.spyOn(player, 'move');
+        player.move(2, 4);
+        expect(spy).toHaveBeenCalledWith(2, 4);
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+});
+
+//mocking
+describe('Player Movement with mocking', () => {
+    it('should move the player', () => {
+       const mock = vi.fn().mockImplementation((x, y) => {
+           console.log({x, y});
+           return { x, y };
+       })
+
+       
+       expect(mock(8,6)).toEqual({x: 8, y: 6});
+       expect(mock).toHaveBeenCalledTimes(1);
+    });
+});
